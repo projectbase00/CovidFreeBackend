9018211d0dbf91cddadd769742025cca056285be
maidenur <ogul.maidenur@gmail.com>
2020-10-30 20:03:19 +0200

    adding the service

---
+++ src/main/java/com/app/covidfree/service/OtpService.java
@@ -0,0 +1,14 @@
+package com.app.covidfree.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OtpService {
+
+    
+    //TODO : add functionality here to send message to phone number
+    public String sendMessage (String phoneNumber){
+        return "123456";
+    }
+    
+}
